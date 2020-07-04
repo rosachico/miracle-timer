bee73b653e94606107565b9861af6353ff6e6029
Sue <46884993+rosachico@users.noreply.github.com>
2020-07-05 00:40:00 +0900

    Add files via upload

---
+++ miracle_timer.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	int key = 0;
+	int i;
+
+	printf("miracle-timer ver.1.0\n");
+	printf("タイマーをセットするには sを入力してEnterキー を押してください。\n");
+	printf("タイマーをセットせずに終了する場合は eを入力してEnterキー を押してください。\n");
+
+	do {
+		if ((key = getchar()) == 's') {
+			printf("タイマーをセットしました\n");
+			Sleep(7200000);	// 2時間待つ
+			printf("");
+			printf("ポケモンが交換されたかもしれません！ミラクルボックスを確認してみましょう。\n");
+			// 5回アラートを鳴らす
+		for (i = 0; i <= 4; i++) {
+			printf("\a");
+			Sleep(1000);
+			}
+		break;
+		}
+		else;
+	}
+	while (key != 'e');	// eが入力されるまでループ
+	return 0;
+}
